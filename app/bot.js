75c2e05b88fcc5f6a48387d3fe04c736136e1ad2
QuaintShanty <quaint.shanty@gmail.com>
2018-08-17 16:46:20 -0400

    Cleaned up the bot script and added !music.

--- app/bot.js
+++ app/bot.js
@@ -1,44 +1,45 @@
-////
-//// Variables
-////
-var essentials    = require.main.require("./app/utils/essentials.js")
-  , conn          = require.main.require("./app/conn.js")
-  , storage       = require.main.require("./app/storage.js")
+// //
+// // Variables
+// //
+const essentials = require.main.require('./app/utils/essentials.js');
+require.main.require('./app/conn.js');
+require.main.require('./app/storage.js');
 
-  , streams       = require.main.require("./app/bot/streams.js");
+const streams = require.main.require('./app/bot/streams.js');
+const music = require.main.require('./app/bot/music.js');
 
-var cmdTrigger = ["!", ".", "?"];
+const cmdTrigger = ['!', '.', '?'];
 
-////
-//// Functions
-////
-function handle(msg) {
-    if( cmdTrigger.includes(msg.content.charAt(0)) ) {
-        handleCommand(msg);
-    }
+// / Determine the command that is wanted to be ran.
+// / Then, run the respective function or functionality.
+function handleCommand(msg) {
+  // Handle the command & params
+  const tempVal = msg.content.indexOf(' ');
+  const command = (msg.content.substring(1, tempVal === -1 ? msg.content.length : tempVal)).toLowerCase();
+  let params = null;
+
+  if (msg.content.includes(' ')) {
+    params = essentials.toParams(msg.content.substring(tempVal + 1, msg.content.length));
+  }
+
+  switch (command) {
+    case 'streams':
+      streams.handle(msg, command, params);
+      break;
+
+    case 'music':
+      music.handle(msg, command, params);
+      break;
+
+    default:
+      break;
+  }
 }
 
-/// Determine the command that is wanted to be ran.
-/// Then, run the respective function or functionality.
-function handleCommand(msg) {
-    // Handle the command & params
-    var tempVal = msg.content.indexOf(' ');
-    var command = (msg.content.substring(1, tempVal == -1 ? msg.content.length : tempVal)).toLowerCase();
-    var params = null;
-    
-    if (msg.content.includes(' ')) {
-        params = essentials.toParams(msg.content.substring(tempVal + 1, msg.content.length));
-    }
-    
-    switch(command) {
-
-        case "streams":
-            streams.handle(msg, command, params);
-            break;
-        
-        default:
-            break;
-    }
+function handle(msg) {
+  if (cmdTrigger.includes(msg.content.charAt(0))) {
+    handleCommand(msg);
+  }
 }
 
 module.exports.handle = handle;
