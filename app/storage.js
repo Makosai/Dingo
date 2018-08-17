6cf00bffb3d5d608b3b5e73ece6156ee34351ee6
QuaintShanty <quaint.shanty@gmail.com>
2018-08-17 16:46:36 -0400

    Cleaned up the storage script and added loading for music.

--- app/storage.js
+++ app/storage.js
@@ -1,51 +1,47 @@
-/////
-// External Reference Variables
-/////
-const fs         = require("fs")
-    , essentials = require.main.require("./app/utils/essentials.js");
+const fs = require('fs');
 
-const conn       = require.main.require("./app/conn.js")
-    , bot        = require.main.require("./app/bot.js")
-    , streams    = require.main.require("./app/bot/streams.js");
+const essentials = require.main.require('./app/utils/essentials.js');
+const conn = require.main.require('./app/conn.js');
+const bot = require.main.require('./app/bot.js');
+const streams = require.main.require('./app/bot/streams.js');
+const music = require.main.require('./app/bot/music.js');
 
-/////
-// Variables
-/////
 
-/////
-// Functions
-/////
-
-// Get the data from the .json file at the filepath.
+/**
+ * Get the data from the .json file at the filepath.
+ * @param {string} filepath
+ * @param {any} defaultData
+ * @param {Function} callback
+ */
 function getData(filepath, defaultData, callback) {
-    essentials.checkForFile(filepath, defaultData ? defaultData : "", function() {
-        fs.readFile(filepath, 'utf8', function(err, contents) {
-            
-            if(contents.length > 0) {
-                callback(JSON.parse(contents));
-            }
-            else {
-                callback(JSON.parse(defaultData));
-            }
-        });
+  essentials.checkForFile(filepath, defaultData || '', () => {
+    fs.readFile(filepath, 'utf8', (err, contents) => {
+      if (contents.length > 0) {
+        callback(JSON.parse(contents));
+      } else {
+        callback(JSON.parse(defaultData));
+      }
     });
+  });
 }
 
 function setData(filepath, defaultData, callback) {
-    essentials.writeFileSync(filepath, defaultData, callback);
+  essentials.writeFileSync(filepath, defaultData, callback);
 }
 
 function load() {
-    // Set an asynchronous looped check to see if conn has loaded. If so, then run onLoad for everything.
-    var interval = setInterval(function() {
-        // If conn is loaded, then the rest should surely be loaded.
-        if(conn.onLoad) {
-            conn.onLoad();
-            //bot.onLoad();
-            streams.onLoad();
-            clearInterval(interval);
-        }
-    }, 10);
+  // Set an asynchronous looped check to see if conn has loaded.
+  //  If so, then run onLoad for everything.
+  const interval = setInterval(() => {
+    // If conn is loaded, then the rest should surely be loaded.
+    if (conn.onLoad) {
+      conn.onLoad();
+      // bot.onLoad();
+      streams.onLoad();
+      music.onLoad();
+      clearInterval(interval);
+    }
+  }, 10);
 }
 
 load();
