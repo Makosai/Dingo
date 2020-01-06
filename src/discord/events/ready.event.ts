e2e4e4deaedf12ab0863da3cf86c081600bf4cbe
QuaintShanty <quaint.shanty@gmail.com>
2020-01-06 01:14:40 -0500

    Updated the ready event to always display who the bot is logged in as.

--- src/discord/events/ready.event.ts
+++ src/discord/events/ready.event.ts
@@ -2,5 +2,5 @@ import { client } from '@discord/discord.config';
 import { debug } from '@utils/essentials.utils';
 
 client.on('ready', () => {
-  debug(`Logged in as ${client.user.tag}`);
+  debug(`Logged in as ${client.user.tag}.`, true);
 });
