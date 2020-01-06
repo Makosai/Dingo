f234267e72e7ef5a74acf831297270f99e662ae1
QuaintShanty <quaint.shanty@gmail.com>
2020-01-06 01:11:05 -0500

    Added a way to handle commands with the bot.

---
+++ src/discord/commands/cmds.main.ts
@@ -0,0 +1,48 @@
+import commandsConfig from './cmds.config';
+import { Message } from 'discord.js';
+import streams from './streams/streams.main';
+
+const { config } = commandsConfig;
+
+async function handleCommand(msg: Message) {
+  // Handle the command & params
+  const tempVal = msg.content.indexOf(' ');
+  const command = msg.content
+    .substring(1, tempVal === -1 ? msg.content.length : tempVal)
+    .toLowerCase();
+  let params: string[] = [];
+
+  if (msg.content.includes(' ')) {
+    params = String.prototype.toParams(
+      msg.content.substring(tempVal + 1, msg.content.length)
+    );
+  }
+
+  switch (command) {
+    case 'help':
+      msg.channel.send(commandsConfig.getCommands());
+      return;
+  }
+
+  if (params.length == 0) {
+    return;
+  }
+
+  switch (command) {
+    case 'streams':
+      streams.handler(params[0], params?.slice(1));
+      return;
+
+    case 'music':
+      return;
+
+    default:
+      return;
+  }
+}
+
+export default async function handle(msg: Message) {
+  if (config.triggers.includes(msg.content.charAt(0))) {
+    handleCommand(msg);
+  }
+}
