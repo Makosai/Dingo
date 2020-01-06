78bed3337e9bb8c746d4f456edf2e85e6498c692
QuaintShanty <quaint.shanty@gmail.com>
2020-01-06 01:05:38 -0500

    Created the core of the application.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Ordered Imports
+import './aliases';
+import './config';
+import '@discord/discord.main';
+
+// Utilities
+import { debug } from '@utils/essentials.utils';
+
+debug('Dingo!!', true);
