86e08570adda7f69312eb90d2979739f707b2762
QuaintShanty <quaint.shanty@gmail.com>
2020-01-07 03:03:49 -0500

    Added a cleanup for webhooks.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,22 @@
 import './aliases';
 import './config';
 import '@discord/discord.main';
+import Twitch from './twitch/twitch.config';
 
 // Utilities
 import { debug } from '@utils/essentials.utils';
+
+process.on('SIGINT', () => {
+  // unsubscribe from all topics
+  Twitch.webhook.unsubscribe('*');
+
+  /* or unsubscribe from each one individually
+  Twitch.webhook.unsubscribe('users/follows', {
+    first: 1,
+    to_id: 12826
+  }); */
+
+  process.exit(0);
+});
 
 debug('Dingo!!', true);
