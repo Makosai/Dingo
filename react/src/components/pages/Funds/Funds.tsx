21242bfe4c31f57678165db908f5c84035aebe29
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-05-12 08:37:56 +0000

    Split the text to make it easier to format with css.

--- react/src/components/pages/Funds/Funds.tsx
+++ react/src/components/pages/Funds/Funds.tsx
@@ -10,23 +10,35 @@ export default function Funds(props: any) {
 
   const [funds, setFunds] = useState(0);
 
-  apiFetch<number>(`funds/${username}`).then((funds) => {
-    setFunds(funds);
-  }).catch(e => {
-    console.log(e);
-  });
+  apiFetch<number>(`funds/${username}`)
+    .then((funds) => {
+      setFunds(funds / 100);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
   useEffect(() => {
     const timer = setInterval(() => {
-      apiFetch<number>(`funds/${username}`).then((funds) => {
-        setFunds(funds);
-      }).catch(e => {
-        console.log(e);
-      });
+      apiFetch<number>(`funds/${username}`)
+        .then((funds) => {
+          setFunds(funds / 100);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }, 10000);
 
     return () => clearInterval(timer);
   }, []);
 
-  return <div id={username}>{`We have currently raised $${funds}`}</div>;
+  return (
+    <div id={username}>
+      <span className="text">{`We have currently raised `}</span>
+      <span className="funds">{`${Number(funds).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      })}`}</span>
+    </div>
+  );
 }
