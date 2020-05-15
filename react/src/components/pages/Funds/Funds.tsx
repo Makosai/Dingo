ec03b6ed73028d1c2cd878ef6f49b879d321ae24
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-05-15 04:24:57 +0000

    Added a name and goal query for the funds page.

--- react/src/components/pages/Funds/Funds.tsx
+++ react/src/components/pages/Funds/Funds.tsx
@@ -8,6 +8,11 @@ import { apiFetch } from '@components/../utils/global_variables';
 export default function Funds(props: any) {
   const username = props.match.params.username;
 
+  const searchParams = new URLSearchParams(props.location.search);
+
+  const name = searchParams.get('name');
+  const goal = searchParams.get('goal');
+
   const [funds, setFunds] = useState(0);
 
   apiFetch<number>(`funds/${username}`)
@@ -34,11 +39,23 @@ export default function Funds(props: any) {
 
   return (
     <div id={username}>
-      <span className="text">{`We have currently raised `}</span>
-      <span className="funds">{`${Number(funds).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      })}`}</span>
+      <span className="text">
+        {name !== null ? `${name}: ` : `We have currently raised `}
+      </span>
+      <span className="funds">
+        {goal !== null && `(`}
+        {`${Number(funds).toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        })}`}
+        {goal !== null && `/`}
+        {goal !== null &&
+          Number(goal).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          })}
+        {goal !== null && `)`}
+      </span>
     </div>
   );
 }
